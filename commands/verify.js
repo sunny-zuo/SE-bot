5b9ac6ac1488712939f4f47bbe878064f2bd27ed
Sunny Zuo <me@sunnyzuo.com>
2021-06-28 08:48:14 -0600

    misc: update text

--- commands/verify.js
+++ commands/verify.js
@@ -29,18 +29,14 @@ module.exports = {
                 .setTitle('Verification Prompt')
                 .setDescription(`[Click here to login using your UWaterloo account to verify.](${process.env.SERVER_URI}/verify/${encodedUID})
                 Authorization allows us to read your profile information to confirm that you are/were a UW student, and you can revoke this permission at any time.
-                If you run into issues, message <@!282326223521316866> for help!
-                
-                **For Applicants:** Unfortunately, you're unable to verify until June when your UW account is activated. Use \`$se26\` in the meantime for a temp role!`));
+                If you run into issues, message <@!282326223521316866> for help!`));
         } else {
             try {
                 message.author.send(new Discord.MessageEmbed().setColor("#00ff00")
                     .setTitle('Verification Prompt')
                     .setDescription(`[Click here to login using your UWaterloo account to verify.](${process.env.SERVER_URI}/verify/${encodedUID})
                 Authorization allows us to read your profile information to confirm that you are/were a UW student, and you can revoke this permission at any time.
-                If you run into issues, message <@!282326223521316866> for help!
-                
-                **For Applicants:** Unfortunately, you're unable to verify until June when your UW account is activated. Use \`$se26\` in the meantime for a temp role!`));
+                If you run into issues, message <@!282326223521316866> for help!`));
                 message.channel.send(`${message.author}, we've DMed you a verification link. Please check your DMs!`);
             } catch (e) {
                 sendErrorEmbed(message.channel, 'Unable to DM Verification Link', 'We seem to be unable to DM you a verification link. Please [temporarily change your privacy settings](https://cdn.discordapp.com/attachments/811741914340393000/820114337514651658/permissions.png) to allow direct messages from server members in order to verify.', message.author);
